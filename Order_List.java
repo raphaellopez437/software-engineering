0998acfb81942e64669f3bcc2e92b94c642e0183
ElBmann <brianrecuero@gmail.com>
2016-11-16 21:23:14 -0500

    Update Order_List.java

--- Order_List.java
+++ Order_List.java
@@ -1,8 +1,8 @@
 /**
- * <p>The class Order List Contains my two usecases. Void Item & ViewWaitingOrder
+ * <p>The class JDBCOrder List Contains my two usecases. Void Item & ViewWaitingOrder
  * Void Item will give the user the ability to void an item after the order
  * has been placed. ViewWaitingOrder allows the User to view all incoming orders
- * that need to be fulfilled.
+ * that need to be fulfilled.The Order_List class will invoke the methods in JDBCOrder_List
  *
  * @author Brian Recuero
  * @version 1.0 Build 1 Nov, 11, 2016
@@ -11,15 +11,16 @@
 
 /**
  * Has several different methods
- *Void Item,OrderList
+ * Void Item,OrderList this Class 
+ * will be invokeing the bridge class Order_List
  *
  */
-public class Order_List {
+public class JDBCOrder_List {
     /**
-     * This method uses aggregation to invoke the delete method from the Item Class.
-     * Then it well be used to delete items after the order has been placed.
+     * This method uses aggregation to invoke the void method from the order_List class.
+     * Then it well be used to void items after the order has been placed.
      * @param Empty
-     * @return will retrun true if there are items to void and false if empty
+     *@return will returns true if there are items to void and false if empty
      */
     public boolean VoidItem() {
 
@@ -27,13 +28,12 @@ public class Order_List {
 
     /**
      * Will Display the list of items in each order from an array list.
-     * Invokes the items added in the item class and is placed in the array.
-     * @param Empty.
-     * @return Will return an array list of orders that are added into the system.
+     * Invokes the items added in the ViewWaitingOrder method in the Order_list class
+     * @param Empty
+     * @return Will return an array list of orders that are added into the system
      *
      */
-    public order[] ViewWaitingOrder( ){
+    public order[] ViewWaitingOrder(){
 
     }
-
 }
