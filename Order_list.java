e2a8c8e563ad335e6665bbe8544767e7b435a799
ElBmann <brianrecuero@gmail.com>
2016-11-16 21:33:53 -0500

    Add files via upload

---
+++ Order_list.java
@@ -0,0 +1,40 @@
+/**
+ * <p>The bridge class Order List contains Void Item & ViewWaitingOrder.
+ * Void Item will give the user the ability to void an item after the order
+ * has been placed. ViewWaitingOrder allows the User to view all incoming orders
+ * that need to be fulfilled.This class will be invoke from the methods in the JDBCOrder_List.
+ *
+ * @author Brian Recuero
+ * @version 1.0 Build 1 Nov, 11, 2016
+ *
+ */
+
+/**
+ * Has several different methods
+ * Void Item,OrderList
+ *
+ */
+public class Order_list {
+    /**
+     * This method will be used to Void items after the order has been placed.
+     *
+     * @param Empty
+     *@return will retrun true if there are items to void and false if empty
+     */
+    public boolean VoidItem() {
+
+    }
+
+    /**
+     * Will Display the list of items in each order from an array list.
+     *
+     * @param Empty
+     * @return Will return an array list of orders that are added into the system
+     *
+     */
+    public order[] ViewWaitingOrder(){
+
+    }
+
+}
+
